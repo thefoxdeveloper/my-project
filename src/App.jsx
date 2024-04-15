8a135be2add97543ec34f4e977ed656eba23dfd9
Douglas Santos <88425982+thefoxdeveloper@users.noreply.github.com>
2024-04-15 19:14:48 -0300

    fix: title change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import CreateUser from "./CreateUser";
 import FilteredCollection from "./FilteredCollection";
 import FlippedCollection from "./FlippedCollection";
